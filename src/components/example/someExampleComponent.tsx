d9816e297a75fd5bc8da69ca616a73a60b2eaeea
Supernuija <joona.manner@gmail.com>
2022-11-02 15:48:51 +0200

    My first fixed component

--- src/components/example/someExampleComponent.tsx
+++ src/components/example/someExampleComponent.tsx
@@ -9,7 +9,7 @@ const SomeExampleComponent: React.FC<SomeExampleComponentProps> = () => {
     return (
         <div className={styles.container}>
             <h1>Header</h1>
-            <p>My supercool component</p>
+            <p>My ultracool component</p>
         </div>
     )
 }
