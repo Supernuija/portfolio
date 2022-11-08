c841ddc4aaf78799c84175d31df59f7652a3f758
Supernuija <joona.manner@gmail.com>
2022-11-08 14:55:29 +0200

    PDF added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import MyFirstComponent from './components/myFirstComponent';
 import SomeExampleComponent from './components/example/someExampleComponent'
-
+import MyPdfFiles from './assets/HUMIDOR.pdf'; //react-app-env.d.ts must be created declare for pdf-type. 
 
 const App: React.FC = () => {
   return (
     <div>
+      <a href={MyPdfFiles}>PDF</a>
       <MyFirstComponent />
       <SomeExampleComponent />
     </div>
