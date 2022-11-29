c26ef27b84b2751aee8b18f0755ba113290578e7
Supernuija <joona.manner@gmail.com>
2022-11-29 15:02:28 +0200

    nyt toimii taas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import MyFirstComponent from './components/myFirstComponent';
+// import MyFirstComponent from './components/myFirstComponent';
 import SomeExampleComponent from './components/example/someExampleComponent'
 import ClickCount from './components/clickCount';
-import Navbar from './navbar/navbar';
+// import Navbar from './navbar/navbar';
 
 const App: React.FC = () => {
   const LogKek = () => {
